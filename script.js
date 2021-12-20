3636c7e46cbd40ec7c47d663e6127ce47f59cf9c
ghazthiskc19 <ghazthiskc.ig@gmail.com>
2021-12-20 16:15:13 +0700

    Adding Media Query up to tablet and hamburger menu

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function scrollFunction() {
         document.querySelector("nav").style.backgroundColor = "transparent";
     }
 }  
+const btnNav = document.querySelector(".nav-checkbox");
+const hamWarp = document.querySelector(".ham-wrapper");
+const navLinks = document.querySelector(".nav-links");
+const navLink = document.querySelectorAll(".nav-link");
+const hamSpan = document.querySelectorAll(".ham-span");
+
+btnNav.addEventListener("click", function(){
+    for (let i = 1; i <= hamSpan.length; i++) {
+        hamSpan[i - 1].classList.toggle(`s${i}-active`);
+    }
+    hamWarp.classList.toggle("colored");
+    navLinks.classList.toggle("animate");
+});
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", function(){
+        for (let i = 1; i <= hamSpan.length; i++) {
+            hamSpan[i - 1].classList.remove(`s${i}-active`);
+        }
+        navLinks.classList.remove("animate");
+        hamWarp.classList.remove("colored");
+    });
+}
